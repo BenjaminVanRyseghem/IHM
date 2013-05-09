796471bb0f51294a87bf5484889c3a6fea2979f0
Wanna-Get-High <franziz.sephiroth@gmail.com>
2013-05-09 21:41:54 +0200

    adding project jTunes

---
+++ MP3Ppayer/Mp3Player/src/player/MusicListModel.java
@@ -0,0 +1,35 @@
+package player;
+
+import java.util.List;
+
+import javax.swing.DefaultListModel;
+
+public class MusicListModel extends DefaultListModel {
+
+	private static final long serialVersionUID = 5945587908050556127L;
+
+	public void addElements(List<String[]> elements) {
+		for (String[] informations : elements) {
+			this.addElement(informations);
+		}
+	}
+	
+	public Object getElementAt(int index) {
+		return this.get(index);
+	}
+	
+	public Object get(int index) {
+		
+		String musicInfo = "";
+		
+		String[] informations = (String[])super.get(index);
+		
+		//System.out.println(index);
+		for (int i = 0; i<informations.length; i++) {
+			//System.out.println(informations[i]);
+			musicInfo += informations[i] + " _ ";
+		}
+		System.out.println(musicInfo);
+		return musicInfo;
+	}
+}
---
+++ MP3Ppayer/Mp3Player/src/player/PlayerView.java
@@ -0,0 +1,103 @@
+package player;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+
+public class PlayerView implements Observer {
+	
+	protected JFrame frame;
+	
+	protected JButton play;
+	protected JButton rewind;
+	protected JButton fastForward;
+	
+	protected JSlider progressBar;
+	
+	protected JTextField search;
+	
+	protected JList musicList;
+	
+	protected ListHandler listHandler;
+	
+	protected PlayerModel model;
+	
+	protected MusicListModel listModel;
+	
+	public PlayerView() {
+		this.model = new PlayerModel();
+		this.model.addObserver(this);
+		this.setUpFrame();
+		
+	}
+	
+	public void setUpFrame() {
+		this.frame = new JFrame("jTunes");
+		this.frame.setLayout(new GridBagLayout());
+		
+		this.setUpButtons();
+		this.setUpSlider();
+		this.setUpSearchField();
+		this.setUpList();
+		
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.pack();
+		this.frame.setVisible(true);
+		
+	}
+	
+	public void setUpButtons() {
+		this.play = new JButton("play/pause");
+		this.rewind = new JButton("rewind");
+		this.fastForward = new JButton("fast forward");
+		
+		this.frame.add(this.rewind,this.constraint(0, 1, 1, 1, GridBagConstraints.CENTER));
+		this.frame.add(this.play,this.constraint(1, 1, 1, 1, GridBagConstraints.CENTER));
+		this.frame.add(this.fastForward,this.constraint(2, 1, 1, 1, GridBagConstraints.CENTER));
+	}
+	
+	public void setUpSlider() {
+		this.progressBar = new JSlider();
+		this.progressBar.setPreferredSize(new Dimension(300, 30));
+		
+		this.frame.add(this.progressBar, this.constraint(0, 0, 3, 1, GridBagConstraints.CENTER));
+	}
+	
+	public void setUpSearchField() {
+		this.search = new JTextField("", 10);
+		
+		this.frame.add(this.search, this.constraint(4, 0, 1, 1, GridBagConstraints.CENTER));
+	}
+	
+	public void setUpList() {
+		this.listModel = new MusicListModel();
+		this.listModel.addElements(this.model.getMusics());
+		
+		this.musicList = new JList(this.listModel);
+		
+		JScrollPane sc = new JScrollPane(this.musicList);
+		sc.setPreferredSize(new Dimension(400,400));
+		
+		this.frame.add(sc, this.constraint(0, 2, 4, 1, GridBagConstraints.CENTER));
+	}
+	
+	public GridBagConstraints constraint(int gridX, int gridY, int gridWidth, int gridHeight, int anchor) {
+		return new GridBagConstraints(gridX, gridY, gridWidth, gridHeight, 0, 0, 
+				anchor, 0, new Insets(0, 0, 0, 0), 0, 0);
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		this.listModel.removeAllElements();
+		this.listModel.addElements(this.model.getMusics());
+	}
+}
